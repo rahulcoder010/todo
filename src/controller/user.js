980d120fc99dad180c117d3152f7ad58e130c37b
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-10-17 14:19:57 +0530

    PB-2 (task) Registration Route

---
+++ src/controller/user.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const User = require('../models/User');
+
+const router = express.Router();
+
+// Register a new user
+router.post('/register', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const user = new User({ username, email, password });
+
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+// Update a user
+router.put('/update/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, email, password } = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(id, {
+      username,
+      email,
+      password
+    }, { new: true });
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+// Update password
+router.put('/update/password/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { password } = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate(id, {
+      password
+    }, { new: true });
+
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+module.exports = router;
---
+++ src/routes/user.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { getUser, createUser } from '../controller/user.js';
+
+const router = express.Router();
+
+router.get('/', getUser);
+router.post('/', createUser);
