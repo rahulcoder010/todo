d25d1a6263e30540da4327c71829fc010884be33
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-07 09:54:27 +0530

    PB-1 (task) test cases for login api

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,86 +1,59 @@
 const express = require('express');
 const User = require('../models/User');
-
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const router = express.Router();
 
-// Register a new user
-router.post('/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+chai.use(chaiHttp);
 
-    const user = new User({ username, email, password });
+describe('Login Endpoint', () => {
+  it('should login a user', (done) => {
+    chai.request(router)
+      .post('/login')
+      .send({ email: 'test@gmail.com', password: 'password' })
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.should.have.property('message').eql('Login successful');
+        done();
+      });
+  });
 
-    await user.save();
+  it('should return an error if email is missing', (done) => {
+    chai.request(router)
+      .post('/login')
+      .send({ password: 'password' })
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.body.should.be.a('object');
+        response.body.should.have.property('message').eql('Email is required');
+        done();
+      });
+  });
 
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+  it('should return an error if password is missing', (done) => {
+    chai.request(router)
+      .post('/login')
+      .send({ email: 'test@gmail.com' })
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.body.should.be.a('object');
+        response.body.should.have.property('message').eql('Password is required');
+        done();
+      });
+  });
 
-// Update a user
-router.put('/update/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { username, email, password } = req.body;
-
-    const updatedUser = await User.findByIdAndUpdate(id, {
-      username,
-      email,
-      password
-    }, { new: true });
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.json(updatedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Update password
-router.put('/update/password/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { password } = req.body;
-
-    const updatedUser = await User.findByIdAndUpdate(id, {
-      password
-    }, { new: true });
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.json(updatedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Login
-router.post('/login', async (req, res) => {
-  try {
-    // Implement login logic here
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Forgot password
-router.post('/forgotpassword', async (req, res) => {
-  try {
-    // Implement forgot password logic here
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
+  it('should return an error if email and password are missing', (done) => {
+    chai.request(router)
+      .post('/login')
+      .send({})
+      .end((error, response) => {
+        response.should.have.status(400);
+        response.body.should.be.a('object');
+        response.body.should.have.property('message').eql('Email and password are required');
+        done();
+      });
+  });
 });
 
 module.exports = router;
