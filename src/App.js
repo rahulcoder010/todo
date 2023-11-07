1f7f7a7c5a79b3b1de229535f0ec036a43c8ff9f
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-07 16:55:02 +0530

    PB-10 (task) 123

---
+++ src/App.js
@@ -0,0 +1,23 @@
+Create or update this file "src/App.js" as needed based on the provided instructions.
+                    The instructions are as follows: create react.js code and write in src/App.js
+
+
+					
+                    The original file contents are referenced below:
+
+```jsx
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my React App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+```
