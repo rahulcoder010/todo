9475d3350e08604e42a61b614fb33482e3debea9
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-07 12:04:56 +0530

    PB-18 (task) test

---
+++ src/controllers/login.js
@@ -0,0 +1,15 @@
+```javascript
+// src/controllers/login.js
+
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      {/* Your login page content */}
+    </div>
+  );
+};
+
+export default Login;
+```
