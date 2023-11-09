24be0e99dd50da312a5217d23c5e913057756247
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-09 09:59:36 +0530

    PB-2 (task) TestCases for login endpoint

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,86 +1,91 @@
-const express = require('express');
-const User = require('../models/User');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
 
-const router = express.Router();
+chai.use(chaiHttp);
+chai.should();
 
-// Register a new user
-router.post('/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+describe('User', () => {
+  describe('POST /register', () => {
+    it('should register a new user', (done) => {
+      chai.request(app)
+        .post('/user/register')
+        .send({
+          username: 'testuser',
+          email: 'testuser@example.com',
+          password: 'testpassword'
+        })
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.an('object');
+          res.body.should.have.property('message').eql('User registered successfully');
+          done();
+        });
+    });
+  });
 
-    const user = new User({ username, email, password });
+  describe('PUT /update/:id', () => {
+    it('should update a user', (done) => {
+      chai.request(app)
+        .put('/user/update/1')
+        .send({
+          username: 'updateduser',
+          email: 'updateduser@example.com',
+          password: 'updatedpassword'
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.property('username').eql('updateduser');
+          res.body.should.have.property('email').eql('updateduser@example.com');
+          done();
+        });
+    });
 
-    await user.save();
+    it('should return a 404 status if user is not found', (done) => {
+      chai.request(app)
+        .put('/user/update/100')
+        .send({
+          username: 'updateduser',
+          email: 'updateduser@example.com',
+          password: 'updatedpassword'
+        })
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.an('object');
+          res.body.should.have.property('message').eql('User not found');
+          done();
+        });
+    });
+  });
 
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
+  describe('PUT /update/password/:id', () => {
+    it('should update the user password', (done) => {
+      chai.request(app)
+        .put('/user/update/password/1')
+        .send({
+          password: 'newpassword'
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.property('password').eql('newpassword');
+          done();
+        });
+    });
+
+    it('should return a 404 status if user is not found', (done) => {
+      chai.request(app)
+        .put('/user/update/password/100')
+        .send({
+          password: 'newpassword'
+        })
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.an('object');
+          res.body.should.have.property('message').eql('User not found');
+          done();
+        });
+    });
+  });
 });
-
-// Update a user
-router.put('/update/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { username, email, password } = req.body;
-
-    const updatedUser = await User.findByIdAndUpdate(id, {
-      username,
-      email,
-      password
-    }, { new: true });
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.json(updatedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Update password
-router.put('/update/password/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { password } = req.body;
-
-    const updatedUser = await User.findByIdAndUpdate(id, {
-      password
-    }, { new: true });
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    res.json(updatedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Login
-router.post('/login', async (req, res) => {
-  try {
-    // Implement login logic here
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Forgot password
-router.post('/forgotpassword', async (req, res) => {
-  try {
-    // Implement forgot password logic here
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-module.exports = router;
