a4c83e9f2135f69509c4d644a6e87a06fc6afb09
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-09 10:03:52 +0530

    PB-5 (task) 123

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,86 +1,64 @@
-const express = require('express');
-const User = require('../models/User');
+import React from 'react';
+import axios from 'axios';
 
-const router = express.Router();
-
-// Register a new user
-router.post('/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-
-    const user = new User({ username, email, password });
-
-    await user.save();
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Update a user
-router.put('/update/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { username, email, password } = req.body;
-
-    const updatedUser = await User.findByIdAndUpdate(id, {
-      username,
-      email,
-      password
-    }, { new: true });
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
+class User extends React.Component {
+  registerUser = async (username, email, password) => {
+    try {
+      const response = await axios.post('/register', {
+        username,
+        email,
+        password
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
+  };
 
-    res.json(updatedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
-
-// Update password
-router.put('/update/password/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { password } = req.body;
-
-    const updatedUser = await User.findByIdAndUpdate(id, {
-      password
-    }, { new: true });
-
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
+  updateUser = async (id, username, email, password) => {
+    try {
+      const response = await axios.put(`/update/${id}`, {
+        username,
+        email,
+        password
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
+  };
 
-    res.json(updatedUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+  updatePassword = async (id, password) => {
+    try {
+      const response = await axios.put(`/update/password/${id}`, {
+        password
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
 
-// Login
-router.post('/login', async (req, res) => {
-  try {
-    // Implement login logic here
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+  login = async (username, password) => {
+    try {
+      // Implement login logic here
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
 
-// Forgot password
-router.post('/forgotpassword', async (req, res) => {
-  try {
-    // Implement forgot password logic here
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+  forgotPassword = async (email) => {
+    try {
+      // Implement forgot password logic here
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  };
+}
 
-module.exports = router;
+export default User;
