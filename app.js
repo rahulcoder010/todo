a43afbf093d64fcc9a58d9d21bd8d15540ba423e
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-10 11:09:57 +0530

    PB-40 (task) login test

--- app.js
+++ app.js
@@ -9,11 +9,15 @@ const port = 3000;
 app.use(cors());
 
 const server = app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+ console.log(`Server started on port ${port}`);
 });
 
 const io = socketIO(server);
 
 app.use('/', routes);
+
+app.post('/login', (req, res) => {
+  // code for login module
+});
 
 module.exports = app;
