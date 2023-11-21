9b62f7bdf7c4e7512898c125532b1ac0102f1d82
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-21 18:25:35 +0530

    PB-6 (task) Test Cases

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # todo
 To Do Application
+
+## Cases
+
+1. Create new to-do item
+2. Read all to-do items
+3. Update existing to-do item
+4. Delete a to-do item
--- src/controller/user.js
+++ src/controller/user.js
@@ -22,64 +22,64 @@ router.post('/register', async (req, res) => {
 // Update a user
 router.put('/update/:id', async (req, res) => {
   try {
-    const { id } = req.params;
-    const { username, email, password } = req.body;
+      const { id } = req.params;
+      const { username, email, password } = req.body;
 
-    const updatedUser = await User.findByIdAndUpdate(id, {
-      username,
-      email,
-      password
-    }, { new: true });
+      const updatedUser = await User.findByIdAndUpdate(id, {
+          username,
+          email,
+          password
+      }, { new: true });
 
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
-    }
+      if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found' });
+      }
 
-    res.json(updatedUser);
+      res.json(updatedUser);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
   }
 });
 
 // Update password
 router.put('/update/password/:id', async (req, res) => {
   try {
-    const { id } = req.params;
-    const { password } = req.body;
+      const { id } = req.params;
+      const { password } = req.body;
 
-    const updatedUser = await User.findByIdAndUpdate(id, {
-      password
-    }, { new: true });
+      const updatedUser = await User.findByIdAndUpdate(id, {
+          password
+      }, { new: true });
 
-    if (!updatedUser) {
-      return res.status(404).json({ message: 'User not found' });
-    }
+      if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found' });
+      }
 
-    res.json(updatedUser);
+      res.json(updatedUser);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
   }
 });
 
 // Login
 router.post('/login', async (req, res) => {
   try {
-    // Implement login logic here
+      // Implement login logic here
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
   }
 });
 
 // Forgot password
 router.post('/forgotpassword', async (req, res) => {
   try {
-    // Implement forgot password logic here
+      // Implement forgot password logic here
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server error' });
+      console.error(error);
+      res.status(500).json({ message: 'Server error' });
   }
 });
 
