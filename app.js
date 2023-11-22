edc6568303c96c2e4f9ee104d21f0c5788fd0458
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-22 11:49:31 +0530

    PB-456 (task) 123678901234567890

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const socketIO = require('socket.io');
 const routes = require('./src/routes/index');
-
 const app = express();
 const port = 3000;
-
 app.use(cors());
-
 const server = app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
 const io = socketIO(server);
-
 app.use('/', routes);
-
 module.exports = app;
--- src/controller/user.js
+++ src/controller/user.js
@@ -67,6 +67,18 @@ router.put('/update/password/:id', async (req, res) => {
 router.post('/login', async (req, res) => {
   try {
     // Implement login logic here
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    if (password !== user.password) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    res.json({ message: 'Login successful' });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Server error' });
