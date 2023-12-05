ddbf380b2ffe6e6363f6f46dffc7fe0cabd8fbbb
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-05 17:59:21 +0530

    PB-3 (task) calc

---
+++ calc.js
@@ -0,0 +1,26 @@
+```javascript
+// add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// subtract two numbers
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// divide two numbers
+function divide(a, b) {
+    return a / b;
+}
+
+// calculate the square root of a number
+function squareRoot(a) {
+    return Math.sqrt(a);
+}
+```
