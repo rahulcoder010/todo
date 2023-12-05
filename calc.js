6fd141f925d284004d3518415501900390fa3ab5
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-05 17:53:26 +0530

    PB-3 (task) calc

---
+++ calc.js
@@ -0,0 +1,27 @@
+// calc.js
+
+// Add function
+function add(a, b) {
+    return a + b;
+}
+
+// Subtract function
+function subtract(a, b) {
+    return a - b;
+}
+
+// Multiply function
+function multiply(a, b) {
+    return a * b;
+}
+
+// Divide function
+function divide(a, b) {
+    return a / b;
+}
+
+// Example usage
+console.log(add(2, 3)); // Output: 5
+console.log(subtract(5, 2)); // Output: 3
+console.log(multiply(4, 6)); // Output: 24
+console.log(divide(10, 2)); // Output: 5
