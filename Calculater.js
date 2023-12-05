cb9a81946a2536325ac760d887c8c7f14329a04d
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-05 11:35:59 +0530

    PB-4 (task) calc

---
+++ Calculater.js
@@ -0,0 +1,15 @@
+function add(num1, num2) {
+    // TODO: write code to add num1 and num2
+}
+
+function subtract(num1, num2) {
+    // TODO: write code to subtract num2 from num1
+}
+
+function multiply(num1, num2) {
+    // TODO: write code to multiply num1 and num2
+}
+
+function divide(num1, num2) {
+    // TODO: write code to divide num1 by num2
+}
