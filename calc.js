95913729e413f3056f79aeb2704e43025fad3592
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-05 17:57:34 +0530

    PB-3 (task) calc

---
+++ calc.js
@@ -0,0 +1,21 @@
+// calc.js
+
+// Add function
+function add(a, b) {
+  return a + b;
+}
+
+// Subtract function
+function subtract(a, b) {
+  return a - b;
+}
+
+// Multiply function
+function multiply(a, b) {
+  return a * b;
+}
+
+// Divide function
+function divide(a, b) {
+  return a / b;
+}
