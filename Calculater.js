4df33a766c62848815d15a68f7080541af59916b
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-05 11:15:25 +0530

    PB-4 (task) calc

---
+++ Calculater.js
@@ -0,0 +1,21 @@
+// Placeholder for the code for creating a calculator in JavaScript
+
+// Add function
+function add(a, b) {
+  return a + b;
+}
+
+// Subtract function
+function subtract(a, b) {
+  return a - b;
+}
+
+// Multiply function
+function multiply(a, b) {
+  return a * b;
+}
+
+// Divide function
+function divide(a, b) {
+  return a / b;
+}
