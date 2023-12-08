50d492f8f4c6ed08c1b593fb3e2f6b70cbcd3c7c
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-08 14:53:25 +0530

    PB-1 (task) test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const socketIO = require('socket.io');
 const routes = require('./src/routes/index');
+const request = require('supertest');
 
 const app = express();
 const port = 3000;
@@ -15,5 +16,40 @@ const server = app.listen(port, () => {
 const io = socketIO(server);
 
 app.use('/', routes);
+
+// Mock data for testing
+const mockData = {
+  id: 1,
+  name: 'John Doe',
+  email: 'johndoe@example.com'
+};
+
+// Test for Home Page
+
+describe('GET /', () => {
+  it('should return status code 200', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+  });
+});
+
+// Test for API routes
+
+describe('API Routes', () => {
+  it('should return status code 200', async () => {
+    const response = await request(app).get('/api/user');
+    expect(response.status).toBe(200);
+  });
+
+  it('should return a valid user object', async () => {
+    const response = await request(app).get('/api/user');
+    expect(response.body).toEqual(mockData);
+  });
+
+  it('should return status code 404 for non-existing route', async () => {
+    const response = await request(app).get('/api/invalid');
+    expect(response.status).toBe(404);
+  });
+});
 
 module.exports = app;
