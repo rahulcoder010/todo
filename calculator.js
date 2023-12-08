76549a256868d9280e9fc7d595986d5291a81186
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-08 13:05:33 +0530

    PB-1 (task) calculator

---
+++ calculator.js
@@ -0,0 +1,29 @@
+// calculator.js
+
+// function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// function to subtract two numbers
+function subtract(a, b) {
+  return a - b;
+}
+
+// function to multiply two numbers
+function multiply(a, b) {
+  return a * b;
+}
+
+// function to divide two numbers
+function divide(a, b) {
+  return a / b;
+}
+
+// export the functions
+module.exports = {
+  add,
+  subtract,
+  multiply,
+  divide,
+};
