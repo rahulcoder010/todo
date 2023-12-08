0736d044dc956381acd03660c27e79af923bee06
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-08 13:04:11 +0530

    PB-2 (task) home page for hospital web

---
+++ home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to our Hospital</h1>
+        <p>Discover the best healthcare services provided by our experienced doctors and nurses.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
