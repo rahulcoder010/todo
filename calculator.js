c49d6f54a4957a51f88e48c4022fc7837bbb2cf1
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-08 12:58:53 +0530

    PB-1 (task) calculator

---
+++ calculator.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const Calculator = () => {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleNum1Change = (e) => {
+    setNum1(e.target.value);
+  };
+
+  const handleNum2Change = (e) => {
+    setNum2(e.target.value);
+  };
+
+  const add = () => {
+    setResult(Number(num1) + Number(num2));
+  };
+
+  const subtract = () => {
+    setResult(Number(num1) - Number(num2));
+  };
+
+  const multiply = () => {
+    setResult(Number(num1) * Number(num2));
+  };
+
+  const divide = () => {
+    setResult(Number(num1) / Number(num2));
+  };
+
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <input type="number" value={num1} onChange={handleNum1Change} />
+      <input type="number" value={num2} onChange={handleNum2Change} />
+
+      <button onClick={add}>Add</button>
+      <button onClick={subtract}>Subtract</button>
+      <button onClick={multiply}>Multiply</button>
+      <button onClick={divide}>Divide</button>
+
+      <h2>Result: {result}</h2>
+    </div>
+  );
+};
+
+export default Calculator;
