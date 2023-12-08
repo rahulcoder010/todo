29bf97fb007efc99725ca1483ac89624a58f77f7
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-08 13:01:20 +0530

    PB-1 (task) calculator

---
+++ calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Calculator</h1>
+
+      {/* Calculator code goes here */}
+    </div>
+  );
+}
+
+export default App;
