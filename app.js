44ff3631173cd013acce00e0f60ee583efce3b7a
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-11 11:46:11 +0530

    PB-1 (task) test

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const routes = require('./src/routes/index');
 const app = express();
 const port = 3000;
 
+// Add "app.use(cors());" below "const app = express();"
 app.use(cors());
 
 const server = app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
+// Add "const io = socketIO(server);" below "const server = app.listen(port, () => {"
 const io = socketIO(server);
 
 app.use('/', routes);
